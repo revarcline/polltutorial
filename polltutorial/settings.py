ca88d6203302f5c67c4cd7ffa26c1e7cefbc7cc9
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-13 16:10:47 -0500

    my models!

--- polltutorial/settings.py
+++ polltutorial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0h=(f&)-156)e^389kl8x6f%azy2%4^v1=q=(d+)&g9^&zhx+0'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
