2ce59ba629dd212ddca4007a059ef8193337ab4e
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-13 16:18:25 -0500

    integrated models

--- polls/models.py
+++ polls/models.py
@@ -9,4 +9,4 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.integerField(default=0)
+    votes = models.IntegerField(default=0)
