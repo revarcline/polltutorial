8f412397449d2d91bbaefaf7fd8d7ed748bf455c
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-13 19:47:21 -0500

    index view tests catching a typo

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,8 @@
 import datetime
 
+from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
-from django.urls import reverse
 
 from .models import Question
 
