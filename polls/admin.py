4e41d574913bee563bf34fd5dd92da0deedd4cc2
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-13 20:21:46 -0500

    more admin improvements

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -7,6 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 
 admin.site.register(Question, QuestionAdmin)
